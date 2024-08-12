24ffee52380c024ff6149ee71d0f1423a4fdbee6
Kauê Matheus <kauematheus7@hotmail.com>
2024-08-12 17:28:02 -0300

    transições

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const elementsToAnimate = document.querySelectorAll('p');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.2 // Ajuste conforme necessário
+    });
+
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
+});
